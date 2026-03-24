103b3f4259dd9e2678b1f9e8a3325e693d3f3625
BosungJu <joobs1127@gmail.com>
2026-03-24 10:52:51 +0900

    feat: folder setting.

---
+++ Assets/Scripts/CsvParser/CsvParaser.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class CsvParaser
+{
+    readonly static string cadData = "CadData/cadData.csv";
+
+    // TODO parse csv data.
+}
---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    // TODO cad data get. cvs 예정.
+
+    public void SetMap(string s)
+    {
+        
+    }
+
+    public void GenerateMap()
+    {
+        
+    }
+}
